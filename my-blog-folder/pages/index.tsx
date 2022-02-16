fc4fae4f4190f7492cd4eeee6fa1dd3ddcbe9dfe
Lautaro <lautyylazzerii@gmail.com>
2022-02-16 14:16:59 -0300

    Header component

--- my-blog-folder/pages/index.tsx
+++ my-blog-folder/pages/index.tsx
@@ -3,13 +3,27 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Personal Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5 '>
+          <h1 className="text-6xl max-w-xl font-serif ">
+            <span className='underline decoration-green-600 decoration-4'>Welcome</span> to my new Blog, a place to show interesting things.
+          </h1>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Earum reiciendis voluptate omnis. Delectus, quis eius!
+          </h2>
+        </div>
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+      </div>
+
     </div>
   )
 }
