a2e52b2aa588001843dd612c412ea1b64f548dae
Lautaro <lautyylazzerii@gmail.com>
2022-02-18 14:17:41 -0300

    BackEnd posts

--- my-blog-folder/pages/index.tsx
+++ my-blog-folder/pages/index.tsx
@@ -1,6 +1,12 @@
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-export default function Home() {
+interface Props {
+  posts: [ Post ];
+}
+
+export default function Home( {posts}: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
@@ -21,3 +27,25 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps = async() => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
