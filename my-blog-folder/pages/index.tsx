38b8967ea013cef0bb0969d5fa88fc7e7e839f38
Lautaro <lautyylazzerii@gmail.com>
2022-02-18 15:04:53 -0300

    FrontEnd

--- my-blog-folder/pages/index.tsx
+++ my-blog-folder/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity';
 import { Post } from '../typings';
@@ -24,8 +25,38 @@ export default function Home( {posts}: Props) {
         <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
       </div>
 
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6">
+        {posts.map((post) => {
+          return (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="group cursor-pointer overflow-hidden rounded-lg border">
+                <img
+                  className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between bg-white p-5">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p>
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div> 
+
     </div>
-  )
+  );
 }
 
 export const getServerSideProps = async() => {
